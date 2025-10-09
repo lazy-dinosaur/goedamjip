986900041a4ce6782ee24ada48709f309d206079
lazydino <lazydino1314@gmail.com>
2025-10-09 20:19:17 +0900

    Feat: 비주얼 효과들 추가

--- lib/visual/visualEffectLibrary.ts
+++ lib/visual/visualEffectLibrary.ts
@@ -1,7 +1,21 @@
 import { EFFECT_FLASHLIGHT } from "./effects/effectFlashlight";
+import { OVERLAY_FOG } from "./effects/overlayFog";
+import { OVERLAY_GLITCH } from "./effects/overlayGlitch";
+import { SCREEN_BLUR } from "./effects/screenBlur";
+import { SCREEN_FLICKER } from "./effects/screenFlicker";
+import { SCREEN_SHAKE } from "./effects/screenShake";
+import { VISUAL_CANDLE_FLICKER } from "./effects/visualCandleFlicker";
+import { VISUAL_VIGNETTE } from "./effects/visualVignette";
 
 const VisualEffectLibrary = {
 	EFFECT_FLASHLIGHT,
+	OVERLAY_FOG,
+	OVERLAY_GLITCH,
+	SCREEN_BLUR,
+	SCREEN_FLICKER,
+	SCREEN_SHAKE,
+	VISUAL_CANDLE_FLICKER,
+	VISUAL_VIGNETTE,
 };
 
 export default VisualEffectLibrary;
