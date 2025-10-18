faa5a632915473e9f9ce4738f87b9051ce5d00a9
lazydino <lazydino1314@gmail.com>
2025-10-18 14:09:45 +0900

    Feat: 이야기 재생 라우트 생성중

---
+++ feature/StoryClient/components/StoryTitle.tsx
@@ -0,0 +1,106 @@
+import ComponentWrapper from "@/component/ComponentWrapper";
+import { LoadingProgress } from "@/feature/MainClient/components/MainTitle/LoadingProgress";
+import { StoryMetadata } from "@/types/story.types";
+import { useGSAP } from "@gsap/react";
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import TextEffect from "@/lib/text/textEffect";
+
+interface StoryTitleProps {
+	loadingProgress: number;
+	title: string;
+	metadata: StoryMetadata;
+	isAssetLoaded: boolean;
+	changeStage: () => void;
+}
+export default function StoryTitle({
+	loadingProgress,
+	title,
+	isAssetLoaded,
+	metadata,
+	changeStage,
+}: StoryTitleProps) {
+	const titleRef = useRef<HTMLDivElement>(null);
+	const textEffectRef = useRef<TextEffect>(null);
+	const tagRefs = useRef<Map<number, HTMLSpanElement>>(new Map());
+	const { tags } = metadata;
+
+	useGSAP(() => {
+		gsap.set(titleRef.current, { opacity: 0 });
+
+		if (titleRef.current && tagRefs.current.size > 0) {
+			const textEffect = new TextEffect();
+			const tagElements = Array.from(tagRefs.current.values());
+
+			textEffect.addEffect("INK_REVEAL", titleRef.current).play();
+			textEffect
+				.getTimeline()
+				.add(
+					gsap.fromTo(
+						tagElements,
+						{
+							opacity: 0,
+							y: 15,
+						},
+						{
+							opacity: 1,
+							y: 0,
+							duration: 0.6,
+							stagger: 0.1,
+							ease: "power2.out",
+							delay: 0.5, // 타이틀 후에 등장
+						},
+					),
+					"-=0.8",
+				)
+				.play();
+
+			textEffectRef.current = textEffect;
+		}
+	}, []);
+
+	const [isClicked, setIsClicked] = useState(false);
+
+	const onClick = () => {
+		setIsClicked(true);
+		textEffectRef.current
+			?.getTimeline()
+			?.eventCallback("onReverseComplete", changeStage)
+			.reverse();
+	};
+
+	return (
+		<ComponentWrapper
+			onClick={isAssetLoaded && !isClicked ? onClick : undefined}
+			className="space-y-10 justify-center h-full"
+			style={{ cursor: isAssetLoaded && !isClicked ? "pointer" : "default" }}
+		>
+			<div
+				ref={titleRef}
+				className="text-5xl md:text-6xl lg:text-7xl text-red-700 text-center px-10 font-bm-euljiro-10"
+			>
+				{title}
+			</div>
+			<div className="space-x-6 text-neutral-300 text-lg md:text-2xl font-bm-hanna-11 text-center">
+				{tags.map((tag, index) => {
+					return (
+						<span
+							ref={(e) => {
+								if (e) tagRefs.current.set(index, e);
+							}}
+							className="whitespace-nowrap"
+							key={`${tag}-${index}`}
+						>
+							#{tag}
+						</span>
+					);
+				})}
+			</div>
+			<LoadingProgress
+				loadingProgress={loadingProgress}
+				visible={!isClicked}
+				location="top"
+			/>
+		</ComponentWrapper>
+	);
+}
---
+++ feature/StoryClient/index.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useLoadingProgress } from "@/hooks/useLoading";
+import { GetAssetsMap } from "@/lib/supabase/asset";
+import { ProcessedSegment } from "@/types/script.types";
+import { useCallback, useEffect, useState } from "react";
+import ReactivationModal from "@/component/ReactivationModal";
+import audioManager from "@/lib/audio/audioManager";
+import { StoryMetadata } from "@/types/story.types";
+import StoryTitle from "./components/StoryTitle";
+
+interface StoryClientProps {
+	assets: GetAssetsMap;
+	script: ProcessedSegment[];
+	title: string;
+	metadata: StoryMetadata;
+	storyId: number;
+}
+
+export default function StoryClient({
+	assets,
+	script,
+	title,
+	metadata,
+	storyId,
+}: StoryClientProps) {
+	const [currentStage, setCurrentStage] = useState<"title" | "story">("title");
+
+	const [currentSegment, setCurrentSegment] = useState(0); // currentSegment를 상위로 이동
+
+	const changeStage = useCallback((stage: "title" | "story") => {
+		setCurrentStage(stage);
+		// title로 돌아갈 때 currentSegment를 리셋할 수 있음
+		if (stage === "title") {
+			setCurrentSegment(0);
+		}
+	}, []);
+	const { loadingProgress, isAssetLoaded } = useLoadingProgress({
+		assets,
+		script,
+	});
+
+	const [needsReactivation, setNeedsReactivation] = useState(false);
+	const [needsRecover, setNeedsRecover] = useState(false);
+	audioManager.setGlobalVolume(0.6);
+
+	useEffect(() => {
+		setNeedsRecover(true);
+	}, [script]);
+
+	return (
+		<>
+			{currentStage == "title" && (
+				<StoryTitle
+					loadingProgress={loadingProgress}
+					isAssetLoaded={isAssetLoaded}
+					changeStage={() => {
+						changeStage("story");
+					}}
+					metadata={metadata}
+					title={title}
+				/>
+			)}
+			{currentStage == "story" && <>story</>}
+
+			<ReactivationModal
+				needsReactivationState={[needsReactivation, setNeedsReactivation]}
+				onReactivationStateChange={(isReactivated) => {
+					if (!isReactivated) {
+						// 재활성화되었을 때 복구 필요
+						setNeedsRecover(true);
+					}
+				}}
+			/>
+		</>
+	);
+}
