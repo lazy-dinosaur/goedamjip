f4bf088d8282a2e7c7447392311ec8f6ea3bcf9f
lazydino <lazydino1314@gmail.com>
2025-10-20 00:56:11 +0900

    Fix:텍스트 밑줄 효과 개선

--- lib/text/effects/styleUnderline.tsx
+++ lib/text/effects/styleUnderline.tsx
@@ -2,30 +2,35 @@ import gsap from "gsap";
 import { EffectCreator } from "../textEffectRegistry";
 
 export const STYLE_UNDERLINE: EffectCreator = (element, options = {}) => {
-	const duration = options.duration ?? 0.6;
 	const tl = gsap.timeline();
 
+	// element를 relative로 설정
 	gsap.set(element, {
 		position: "relative",
 		display: "inline-block",
 	});
 
+	// ::after 처럼 underline element 생성
 	const underline = document.createElement("span");
 	underline.style.position = "absolute";
 	underline.style.left = "0";
-	underline.style.bottom = "0em";
+	underline.style.bottom = "0";
 	underline.style.width = "100%";
 	underline.style.height = "2px";
 	underline.style.backgroundColor = "currentColor";
-	underline.style.transformOrigin = "left";
 
 	element.appendChild(underline);
 
-	gsap.set(underline, { scaleX: 0 });
+	// 처음에는 scaleX 0으로 설정 (왼쪽에서 시작)
+	gsap.set(underline, {
+		scaleX: 0,
+		transformOrigin: "left center",
+	});
 
+	// timeline에 왼쪽에서 오른쪽으로 그어지는 애니메이션 추가
 	tl.to(underline, {
-		duration,
 		scaleX: 1,
+		duration: 0.6,
 		ease: "power2.out",
 	});
 
