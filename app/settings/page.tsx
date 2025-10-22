a7b162a2a9d6408d5aa4901903746ce1ab0a17ef
lazydino <lazydino1314@gmail.com>
2025-10-22 14:34:37 +0900

    Feat: 설정화면 기능 구현 완료

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-import PageWrapper from "@/component/PageWrapper";
 import ScreenWrapper from "@/component/ScreenWrapper";
+import PageWrapper from "@/component/PageWrapper";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Slider } from "@/components/ui/slider";
 import { useSettingsContext } from "@/contexts/SettingsContext";
 import { useRouter } from "next/navigation";
+import { skip } from "node:test";
 import { FaChevronLeft } from "react-icons/fa6";
 
 export default function Settings() {
@@ -18,29 +21,87 @@ export default function Settings() {
 		setMasterVolume,
 		resetToDefault,
 	} = useSettingsContext();
+
 	return (
-		<ScreenWrapper>
-			<div className="fixed mx-auto max-w-screen-xl inset-0 p-8 ">
-				<FaChevronLeft className="text-4xl" />
+		<ScreenWrapper className="flex flex-col">
+			<div className="p-5 md:p-8 flex w-full text-4xl space-x-5">
+				<FaChevronLeft
+					onClick={() => {
+						router.back();
+					}}
+					className="text-xl md:text-4xl font-medium cursor-pointer"
+				/>
+				<span className="text-xl md:text-4xl font-medium font-bm-hanna-11 w-[15%]">
+					설정
+				</span>
 			</div>
-			<PageWrapper>
-				<ul className="w-full px-20">
-					<li className="w-full">
-						<label
-							htmlFor="large-range"
-							className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-						>
-							Large range
-						</label>
-						<input
-							id="large-range"
-							type="range"
-							min="0"
-							max="5"
-							value="2.5"
-							step="0.5"
-							className="w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-700"
+			<PageWrapper className="justify-start flex-1">
+				<ul className="w-full p-1 py-5 md:p-20 flex flex-col space-y-5 md:space-y-24 max-h-full h-full overflow-y-auto">
+					<li className="w-full flex items-center space-x-3">
+						<span className="md:text-2xl font-medium font-bm-hanna-11 w-[40%]">
+							전체 볼륨
+						</span>
+						<Slider
+							defaultValue={[masterVolume * 100]}
+							onValueChange={(e) => {
+								setMasterVolume(Number((e[0] / 100).toFixed(2)));
+							}}
+							max={100}
+							step={1}
 						/>
+						<span className="md:text-xl font-bold">
+							{(masterVolume * 100).toFixed(0)}
+						</span>
+					</li>
+
+					<li className="w-full flex items-center space-x-3">
+						<span className="md:text-2xl font-medium font-bm-hanna-11 w-[40%]">
+							환경음
+						</span>
+						<Slider
+							defaultValue={[ambienceVolume * 100]}
+							onValueChange={(e) => {
+								setAmbienceVolume(Number((e[0] / 100).toFixed(2)));
+							}}
+							max={100}
+							step={1}
+						/>
+						<span className="md:text-xl font-bold">
+							{(ambienceVolume * 100).toFixed(0)}
+						</span>
+					</li>
+					<li className="w-full flex items-center space-x-3">
+						<span className="md:text-2xl font-medium font-bm-hanna-11 w-[40%]">
+							효과음
+						</span>
+						<Slider
+							defaultValue={[sfxVolume * 100]}
+							onValueChange={(e) => {
+								setSfxVolume(Number((e[0] / 100).toFixed(2)));
+							}}
+							max={100}
+							step={1}
+						/>
+						<span className="md:text-xl font-bold">
+							{(sfxVolume * 100).toFixed(0)}
+						</span>
+					</li>
+					<li className="w-full flex items-center space-x-3">
+						<span className="md:text-2xl font-medium font-bm-hanna-11 w-[25%]">
+							인트로 스킵
+						</span>
+						<div className="flex flex-1 justify-center items-center">
+							<Checkbox
+								defaultChecked={skipIntro}
+								onCheckedChange={(e) => {
+									setSkipIntro(Boolean(e));
+								}}
+								className="size-4 md:size-6"
+							/>
+						</div>
+						<span className="md:text-xl font-bold w-[15%] flex justify-end">
+							{skipIntro ? "안보기" : "보기"}
+						</span>
 					</li>
 				</ul>
 			</PageWrapper>
