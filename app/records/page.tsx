ec9d54c55105329961f59f8dcea7eeb69b5b9756
lazydino <lazydino1314@gmail.com>
2025-10-24 14:16:24 +0900

    Feat: 이야기 리스트 페이지 개발중

--- app/records/page.tsx
+++ app/records/page.tsx
@@ -1,3 +1,17 @@
+import PageWrapper from "@/component/PageWrapper";
+import ScreenWrapper from "@/component/ScreenWrapper";
+import { FaChevronLeft } from "react-icons/fa6";
+
 export default function Records() {
-	return <div>이야기 리스트</div>;
+	return (
+		<ScreenWrapper className="flex flex-col">
+			<div className="p-5 md:p-8 flex w-full text-4xl space-x-6">
+				<FaChevronLeft className="text-xl md:text-4xl font-medium cursor-pointer" />
+				<span className="text-xl md:text-4xl font-medium font-bm-hanna-11 w-[15%]">
+					괴담집
+				</span>
+			</div>
+			<PageWrapper className="justify-start flex-1"></PageWrapper>
+		</ScreenWrapper>
+	);
 }
